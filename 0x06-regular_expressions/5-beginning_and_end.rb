44e4bc0df2d801b8d6fbd953da2dbd6b017dbb17
UserBryan <bkk@gmail.com>
2024-01-30 05:01:13 -0800

    Not quite HBTN yet

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts (ARGV[0] =~ /^h.n$/ ? ARGV[0] : "$")
+# .scan(/^h.n$/): This method is used to scan the input string for
+# occurrences of the specified regular expression. The regular expression
+# ^h.n$ breaks down as follows:
+# ^: Anchors the regex at the beginning of the string.
+# h: Matches the character "h" literally.
+# .: Matches any single character.
+# n: Matches the character "n" literally.
+# $: Anchors the regex at the end of the string.
+puts ARGV[0].scan(/^h.n$/).join
