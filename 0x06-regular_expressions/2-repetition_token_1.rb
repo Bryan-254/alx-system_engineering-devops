003913e64159bcb2aedaa05f24030fcaaa3c3c99
UserBryan <bkk@gmail.com>
2024-01-30 04:07:45 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb?tn/).join
+
+# .scan(/hb*t?n/): This method is used to scan the input string for
+# occurrences of the specified regular expression. The regular expression
+# /hb*t?n/ breaks down as follows:
+# h: Matches the character "h" literally.
+# b*: Matches zero or more occurrences of the character "b."
+# t?: Matches zero or one occurrence of the character "t."
+# n: Matches the character "n" literally.
+
+puts ARGV[0].scan(/hb*t?n/).join
