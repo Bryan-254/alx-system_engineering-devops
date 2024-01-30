03801398814ef9a58a1b00cc2f454d40590f4c58
UserBryan <bkk@gmail.com>
2024-01-30 03:10:31 -0800

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
+
+# ARGV[0] refers to the first argument, if it exists.
+# In this case, it's the input string that the script will operate on.
+
+# .scan(/School/): This method is used on the input string to
+# scan for occurrences of the regular expression /School/
+
+# .join: This method is used to concatenate the matches found by
+# scan into a single string.
+
+# If there are multiple occurrences, they will be concatenated into
+# a single string and printed. If there are no matches, nothing will
+# be printed.
+
+# puts: This method is used to print the result to the standard output.
 puts ARGV[0].scan(/School/).join
