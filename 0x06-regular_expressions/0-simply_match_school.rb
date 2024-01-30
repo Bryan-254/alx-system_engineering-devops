3272807066fe5d1274bd97f45a8f7b2a8f80aaf5
UserBryan <bkk@gmail.com>
2024-01-30 02:32:07 -0800

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
 
-# Check if an argument was provided
-if ARGV.empty?
-  puts "Please provide a string to match against the school regular expression."
+# Check if the argument contains the word "School" and append it to the end
+def match_school(arg)
+  if (match = arg.match(/School/))
+    puts arg.gsub(/School/, "") + match[0] + "$"
+  else
+    puts "$"
+  end
+end
+
+# Accept one argument from the command line
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
   exit 1
 end
 
-# Define the school regular expression
-school_regex = /\bSchool\b/i
-
-# Get the argument from the command line
-input_string = ARGV[0]
-
-# Check if the input string matches the school regular expression
-if input_string.match?(school_regex)
-  puts "School"
-else
-  puts "$"
-end
+# Pass the argument to the matching method
+match_school(ARGV[0])
