f866b169a563c3551f6c003f87dc5955ad6c1e42
UserBryan <bkk@gmail.com>
2024-01-30 03:39:08 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-
-# .scan(/hb?tn/): This method is used to scan the input string
-# for occurrences of the specified regular expression. The regular
-# expression /hb?tn/ matches strings that start with "hbt" or "hbtn,"
-# where the "?" quantifier allows for zero or one occurrence of the letter "b."
-
 puts ARGV[0].scan(/hb?tn/).join
