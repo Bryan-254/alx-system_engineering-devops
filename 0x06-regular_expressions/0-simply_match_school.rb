9445354f064a6aca1ddcd97c8d79bb616bd72881
UserBryan <bkk@gmail.com>
2024-01-30 02:36:10 -0800

    Simply matching School task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,23 @@
 #!/usr/bin/env ruby
 
-# Check if the argument contains the word "School" and append it to the end
-def match_school(arg)
-  if (match = arg.match(/School/))
-    puts arg.gsub(/School/, "") + match[0] + "$"
-  else
-    puts "$"
-  end
-end
-
-# Accept one argument from the command line
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
+# Check if an argument was provided
+if ARGV.empty?
+  puts "Please provide a string to match against the school regular expression."
   exit 1
 end
 
-# Pass the argument to the matching method
-match_school(ARGV[0])
+# Define the school regular expression
+school_regex = /\bSchool\b/i
+
+# Get the argument from the command line
+input_string = ARGV[0]
+
+# Find all occurrences of the word "School" in the input string
+matches = input_string.scan(school_regex)
+
+# Print out each occurrence of the word "School"
+matches.each do |match|
+  print match, "$"
+end
+
+puts
