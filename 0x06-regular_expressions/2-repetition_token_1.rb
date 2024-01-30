d4da27814390a72764d5d5e9fa87cdbc4ac064eb
UserBryan <bkk@gmail.com>
2024-01-30 04:14:23 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -5,7 +5,6 @@
 # /hb*t?n/ breaks down as follows:
 # h: Matches the character "h" literally.
 # b*: Matches zero or more occurrences of the character "b."
-# t?: Matches zero or one occurrence of the character "t."
+# t: Matches the character "t" literally.
 # n: Matches the character "n" literally.
-
-puts ARGV[0].scan(/hb*t?n/).join
+puts ARGV[0].scan(/hb?tn/).join
